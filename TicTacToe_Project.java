1c7e8d5c33f47e01a7ddbf94bd4f4f670a9c4968
PRIYANK SINGH <priyanksingh5004@gmail.com>
2025-11-09 20:16:59 +0530

    improve draw detection

--- TicTacToe_Project.java
+++ TicTacToe_Project.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class TicTacToe {
 
     public static void printBoard(char[][] board) {
@@ -16,32 +17,38 @@ public class TicTacToe {
         }
     }
 
-    public static boolean isGameOver(char[][] board, char player) {
+    public static boolean hasWon(char[][] board, char player) {
+        // Rows
         for (int i = 0; i < 3; i++) {
             if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
                 return true; 
             }
+        }
+        // Columns
+        for (int i = 0; i < 3; i++) {
             if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
                 return true; 
             }
         }
+        // Diagonals
         if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
             return true; 
         }
         if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
             return true; 
         }
+        return false; 
+    }
 
-        boolean draw = true;
+    public static boolean isDraw(char[][] board) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == ' ') {
-                    draw = false;
-                    break;
+                    return false; 
                 }
             }
         }
-        return draw;
+        return true;
     }
 
     public static void main(String[] args) {
@@ -54,40 +61,31 @@ public class TicTacToe {
         }
         char currentPlayer = 'X'; 
         int row, col;
-
         System.out.println("Welcome to Tic-Tac-Toe!");
-
         while (true) {
             System.out.println("\nCurrent board:");
             printBoard(board);
-
             System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
             row = scanner.nextInt();
             col = scanner.nextInt();
-
             if (row < 1 || row > 3 || col < 1 || col > 3 || board[row - 1][col - 1] != ' ') {
                 System.out.println("Invalid move. Try again.");
                 continue;
             }
-
             board[row - 1][col - 1] = currentPlayer;
-
-            if (isGameOver(board, currentPlayer)) {
+            if (hasWon(board, currentPlayer)) {
                 System.out.println("Current board:");
                 printBoard(board);
-                if (isGameOver(board, 'X')) {
-                    System.out.println("Player X wins!");
-                } else if (isGameOver(board, 'O')) {
-                    System.out.println("Player O wins!");
-                } else {
-                    System.out.println("It's a draw!");
-                }
+                System.out.println("Player " + currentPlayer + " wins!");
                 break;
             }
-
+            else if (isDraw(board)) {
+                System.out.println("Current board:");
+                printBoard(board);
+                System.out.println("It's a draw!");
+                break;
+            }
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
         }
-
-        scanner.close();
     }
 }
